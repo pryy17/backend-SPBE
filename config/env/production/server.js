2c3346b7973af10d0829e90d96171bf0483c0000
pryy17 <priandy170501@gmail.com>
2023-05-29 18:09:12 +0700

    server fix

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
   url: env("RAILWAY_STATIC_URL"),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
 });
